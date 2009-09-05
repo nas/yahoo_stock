e0ccbd465071d82f043f9d8a7f451af3dd98ca5c
Nasir Jamal <nas35_in@yahoo.com>
2009-09-05 22:17:57 +0100

    requires scrip symbol class in the yahoo_stock.rb

--- lib/yahoo_stock.rb
+++ lib/yahoo_stock.rb
@@ -1,9 +1,10 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'yahoo_stock/scrip_symbol.rb'
 require 'yahoo_stock/interface.rb'
 require 'yahoo_stock/quote.rb' 
 
 module YahooStock
-  VERSION = '0.0.1'
+  VERSION = '0.1.3'
 end
