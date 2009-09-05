13444ec630710aed95d1867883945cc9347d6934
Nasir Jamal <nas35_in@yahoo.com>
2009-09-05 22:13:16 +0100

    Changes in YahooStock::ScripSymbol
     * #get_results is now private
     * Complete spec test
     * More comments
     * #parse_results changed to #find

--- lib/yahoo_stock/scrip_symbol.rb
+++ lib/yahoo_stock/scrip_symbol.rb
@@ -1,62 +1,50 @@
-=begin
-
-== DESCRIPTION:
-
-This class provides the ability to find out the stock /scrip symbols for a company used in stock exchanges.
-
-It uses Yahoo http://finance.yahoo.com/lookup page to find, screen scrape / parse the returned results.
-
-== USAGE:
- 
-* If you want to use the symbols in your existing code then:
-
-symbol = YahooStock::ScripSymbol.new('company name')
-
-symbol.parse_results
-
-will give you an array of arrays where each outer array is the different option for the company name
-
-you provided and the inner array includes stock symbol, full company name, stock price, exchange symbol
-
-so that you can decide easily what symbol you can use.
-
-* If you just want to print the results on your console screen
-
-YahooStock::ScripSymbol.print_options('company name')
-
-* If you just want to store the results in file to use it later
-
-You can pass in any number of companies in the parameter
-
-YahooStock::ScripSymbol.save_options_to_file('path/to/filename','company1', 'company2')
-
-=end
-
-
 require 'net/http'
+
 module YahooStock
+  # == DESCRIPTION:
+  # 
+  # This class provides the ability to find out the stock /scrip symbols for a company used in stock exchanges.
+  # 
+  # It uses Yahoo http://finance.yahoo.com/lookup page to find, screen scrape / parse the returned results.
+  # 
+  # == USAGE:
+  #  
+  # * If you want to use the symbols in your existing code then:
+  # 
+  #     symbol = YahooStock::ScripSymbol.new('company name')
+  # 
+  #     symbol.find
+  # 
+  # will give you an array of arrays where each outer array is the different option for the company name
+  # you provided and the inner array includes stock symbol, full company name, stock price, exchange symbol
+  # so that you can decide easily what symbol you can use.
+  # 
+  # * If you just want to print the results on your console screen
+  # 
+  #     YahooStock::ScripSymbol.print_options('company name')
+  # 
+  # * If you just want to store the results in file to use it later
+  # 
+  # You can pass in any number of companies in the parameter
+  # 
+  #     YahooStock::ScripSymbol.save_options_to_file('path/to/filename','company1', 'company2')
+  # 
   class ScripSymbol
-  
+    # Initialize with the name of the company as parameter for which stock symbol is needed
+    # 
+    #     symbol = YahooStock::ScripSymbol.new('company name')
+    # 
+    #     symbol.find
     def initialize(company)
       company = company.gsub(/\s/,'+')
       @base_url = "http://finance.yahoo.com/lookup/all?s=#{company}"
       @before_element = 'yfi_sym_results'
       @after_element = 'yfi_fp_left_bottom'
     end
-  
-    def get_results
-      @body ||= Net::HTTP.get(URI.parse(@base_url)).gsub!(/\s*/,'')
-      pattern = /#{@before_element}.*#{@after_element}/
-      results = pattern.match(@body)
-      if results
-        return results[0]
-      end
-    end
     
     # Returns an array of arrays where each outer array is the different option for the company name
-    # you provided and the inner array includes stock symbol, full company name, stock price, exchange symbol
-    
-    def parse_results
+    # you provided and the inner array includes stock symbol, full company name, stock price, exchange symbol.
+    def find
       data = []
       rows = get_results.to_s.split(/\<\/tr>/)
       rows.each_with_index do |row, row_i|
@@ -72,25 +60,38 @@ module YahooStock
       data
     end
     
-    # This is just a convenience methos to print all results on your console screen 
-    # and to return nil at the end. It uses parse_results method to print symbols on the screen
-    def self.print_options(*company)
-      company.each do |name|
+    # This is just a convenience method to print all results on your console screen 
+    # and to return nil at the end. It uses find method to print symbols on the screen.
+    def self.print_options(*companies)
+      companies.each do |name|
         scrip_symbol = self.new(name)
-        scrip_symbol.parse_results.each {|scrip| p scrip}
+        scrip_symbol.find.each {|scrip| p scrip}
       end
       nil
     end
     
-    def self.save_options_to_file(file_name, *company)
+    # Another convenience method to store all returned results into a file
+    def self.save_options_to_file(file_name, *companies)
       File.open(file_name, 'a') do |f|
-        company.each do |name|
+        companies.each do |name|
           scrip_symbol = self.new(name)
-          scrip_symbol.parse_results.each do |scrip| 
+          scrip_symbol.find.each do |scrip| 
             f.write(scrip.join(', '))
             f.puts('')
           end
         end
+      end
+    end
+    
+    private
+    
+    # Makes an http request to the given url and returns only the text among two points
+    def get_results
+      @body ||= Net::HTTP.get(URI.parse(@base_url)).gsub!(/\s*/,'')
+      pattern = /#{@before_element}.*#{@after_element}/
+      results = pattern.match(@body)
+      if results
+        return results[0]
       end
     end
     
--- spec/yahoo_stock/scrip_symbol_spec.rb
+++ spec/yahoo_stock/scrip_symbol_spec.rb
@@ -2,62 +2,109 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe YahooStock::ScripSymbol do
   
-  describe "get_results" do
+  describe "find" do
     before(:each) do
-      @scrip_symbol = YahooStock::ScripSymbol.new('company')
-      @before_element = 'yfi_sym_results'
-      @after_element = 'yfi_fp_left_bottom'
+      @script_symbol = YahooStock::ScripSymbol.new('company1')
     end
     
-    it "should call get the results from remote url" do
-      Net::HTTP.should_receive(:get).and_return('some string')
-      @scrip_symbol.get_results
+    it "should get results" do
+      @script_symbol.should_receive(:get_results)
+      @script_symbol.find
     end
     
-    it "should parse the URI" do
-      Net::HTTP.stub!(:get).and_return('some string')
-      URI.should_receive(:parse)
-      @scrip_symbol.get_results
+    it "should convert the results to string and split them on the basis of tr tags" do
+      res = 'Some results returned'
+      @script_symbol.stub!(:get_results).and_return(res)
+      res.should_receive(:split).with(/\<\/tr>/).and_return([])
+      @script_symbol.find
     end
     
-    it "should remove any special characters from the returned string using regex" do
-      string = 'some string'
-      Net::HTTP.stub!(:get).and_return(string)
-      string.should_receive(:gsub!).with(/\s*/,'')
-      @scrip_symbol.get_results
+    it "should return an array of options" do
+      @script_symbol.stub!(:get_results).and_return('ss')
+      @script_symbol.find.class.should eql(Array)
     end
-    
-    it "should not have any special characters in the actual returned string" do
-      string = "asd#{@before_element} some string \n another line substitue \t\r remove these as well #{@after_element}"
-      Net::HTTP.stub!(:get).and_return(string)
-      @scrip_symbol.get_results.should eql("#{@before_element}somestringanotherlinesubstitueremovetheseaswell#{@after_element}")
-    end
-    
-    it "should not return anything if it is not between before and after element" do
-      string = "some string \n another line substitue \t\r remove these as well"
-      Net::HTTP.stub!(:get).and_return(string)
-      @scrip_symbol.get_results.should eql(nil)
-    end
-    
-  end
-  
-  describe "parse_results" do
-    
   end
   
   describe ".print_options" do
+    before(:each) do
+      @symbol = stub(YahooStock::ScripSymbol)
+      @script_symbol = YahooStock::ScripSymbol.stub!(:new).and_return(@symbol)
+    end
     
+    it "should find scrip symbol for one company" do
+      @symbol.should_receive(:find).once.and_return([])
+      YahooStock::ScripSymbol.print_options('company1')
+    end
+    
+    it "should find scrip symbols for two companies" do
+      @symbol.should_receive(:find).twice.and_return([])
+      YahooStock::ScripSymbol.print_options('company1', 'company2')
+    end
+    
+    it "should return nil" do
+      @symbol.should_receive(:find).and_return([])
+      YahooStock::ScripSymbol.print_options('company1').should eql(nil)
+    end
+    
+    it "should print the results for each scrip or company, i.e twice" do
+      scrip = ['name','stock']
+      @symbol.stub!(:find).and_return([scrip])
+      YahooStock::ScripSymbol.should_receive(:p).with(scrip).twice
+      YahooStock::ScripSymbol.print_options('company1','company2')
+    end
+    
+    it "should print the results for each scrip or company, 4times" do
+      scrip = ['name','stock']
+      @symbol.stub!(:find).and_return([scrip,scrip])
+      YahooStock::ScripSymbol.should_receive(:p).with(scrip).exactly(4)
+      YahooStock::ScripSymbol.print_options('company1','company2')
+    end
   end
   
-  # def self.print_options(*company)
-  #     company.each do |name|
-  #       scrip_symbol = self.new(name)
-  #       scrip_symbol.parse_results.each {|scrip| p scrip}
-  #     end
-  #     nil
-  #   end
-  
   describe ".save_options_to_file" do
+    before(:each) do
+      @symbol = stub(YahooStock::ScripSymbol)
+      @script_symbol = YahooStock::ScripSymbol.stub!(:new).and_return(@symbol)
+      @file = stub(File)
+      @file_name = 'file_name'
+    end
+    
+    it "should create or open a file with file name parameter and appends to it" do
+      File.should_receive(:open).with(@file_name, 'a')
+      YahooStock::ScripSymbol.save_options_to_file('file_name', 'company1')
+    end
+    
+    it "should find scrip symbol for one company" do
+      @symbol.should_receive(:find).once.and_return([])
+      YahooStock::ScripSymbol.save_options_to_file(@file_name,'company1')
+    end
+
+    it "should find scrip symbols for two companies" do
+      @symbol.should_receive(:find).twice.and_return([])
+      YahooStock::ScripSymbol.save_options_to_file(@file_name, 'company1','company2')
+    end
+    
+    it "should have one option for each company in the file" do
+      scrip = ['name','stock']
+      @symbol.stub!(:find).and_return([scrip])
+      YahooStock::ScripSymbol.save_options_to_file(@file_name, 'company1','company2')
+      file_data.should eql(["name, stock\n", "name, stock\n"])
+    end
+    
+    it "should have two option for each company in the file" do
+      scrip = ['name','stock']
+      @symbol.stub!(:find).and_return([scrip, scrip])
+      YahooStock::ScripSymbol.save_options_to_file(@file_name, 'company1','company2')
+      file_data.should eql(["name, stock\n", "name, stock\n", "name, stock\n", "name, stock\n"])
+    end
+    
+    def file_data
+      File.open(@file_name, 'r') { |f| return f.readlines }
+    end
+    
+    after(:each) do
+      File.delete(@file_name) if File.exists? @file_name
+    end
     
   end
   
