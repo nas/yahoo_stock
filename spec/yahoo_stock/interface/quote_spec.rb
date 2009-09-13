3b02f9c2d46757dff31c7b7d285c445ca793fc14
Nasir Jamal <nas35_in@yahoo.com>
2009-09-13 10:19:24 +0100

    Adds test for get to Interface::Quote spec

--- spec/yahoo_stock/interface/quote_spec.rb
+++ spec/yahoo_stock/interface/quote_spec.rb
@@ -259,4 +259,39 @@ describe YahooStock::Interface::Quote::Quote do
    
   end
   
+  describe "get" do
+    before(:each) do
+      @response = stub('Response')
+      Net::HTTP.stub!(:get_response).and_return(@response)
+      URI.stub!(:parse)
+      @response.stub!(:code).and_return('200')
+      @response.stub!(:body)
+    end
+    
+    it "should find or generate the uri once in the get method and once in the get method of the super class" do
+      @interface.should_receive(:uri).twice
+      @interface.get
+    end
+    
+    it "should get response for the uri" do
+      Net::HTTP.should_receive(:get_response).and_return(@response)
+      @interface.get
+    end
+    
+    it "should parse the uri" do
+      URI.should_receive(:parse)
+      @interface.get
+    end
+    
+    it "should check the response code" do
+      @response.should_receive(:code)
+      @interface.get
+    end
+    
+    it "should get the body of the response if returned code is 200, ie success" do
+      @response.should_receive(:body)
+      @interface.get
+    end
+  end
+  
 end
