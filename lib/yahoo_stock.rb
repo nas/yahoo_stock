1b8938802af167742e252d62f4510bd3f132daaf
Nasir Jamal <nas35_in@yahoo.com>
2009-09-23 06:52:46 +0100

    Update required files in yahoo_stock.rb

--- lib/yahoo_stock.rb
+++ lib/yahoo_stock.rb
@@ -2,15 +2,16 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'date'
-require 'yahoo_stock/scrip_symbol.rb'
+require 'yahoo_stock/base.rb'
 require 'yahoo_stock/interface.rb'
+require 'yahoo_stock/history.rb'
+require 'yahoo_stock/scrip_symbol.rb'
+require 'yahoo_stock/quote.rb' 
 require 'yahoo_stock/interface/quote.rb'
 require 'yahoo_stock/interface/history.rb'
 require 'yahoo_stock/interface/scrip_symbol.rb'
-require 'yahoo_stock/quote.rb' 
-require 'yahoo_stock/history.rb'
-require 'yahoo_stock/result_parser.rb'
+require 'yahoo_stock/result/array_format.rb'
 
 module YahooStock
-  VERSION = '0.1.3'
+  VERSION = '1.0.1'
 end
