c0ccd40b7f183343565d20e928848e6ce5b8c19a
Nasir Jamal <nas35_in@yahoo.com>
2010-11-13 21:57:22 +0000

    Revert "Upgrading to rspec 2.0.1."
    This reverts commit 2fecdd0b49eced0ab68fb339bbf5c121b675f4aa.
    
    Conflicts:
    
    	spec/spec_helper.rb

--- rspec/spec_helper.rb
+++
@@ -1,3 +0,0 @@
-$: << File.join(File.dirname(__FILE__), "/../lib")
-require 'rspec'
-require 'yahoo_stock'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
 $: << File.join(File.dirname(__FILE__), "/../lib") 
+
 require 'rspec'
 require 'yahoo_stock'
